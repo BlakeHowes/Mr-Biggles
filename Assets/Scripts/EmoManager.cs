1d4fd9b9bcd6731ff55539c21a2dfdbc5a6e7149
Melissa <mel.e.moore@outlook.com>
2020-08-04 13:48:28 +1200

    CatModel

---
+++ Assets/Scripts/EmoManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmoManager : MonoBehaviour
+{
+    public enum Emotion {Happy, Idle, IdleTalking};
+    public Emotion currentEmotion = Emotion.Idle;
+
+    public Texture happyTexture;
+    public Texture idleTexture;
+    public Texture idleTalkingTexture;
+
+    private Material mat;
+
+    void Start()
+    {
+        mat = transform.GetChild(0).GetComponent<Renderer>().sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentEmotion)
+        {
+            case Emotion.Happy:
+                mat.SetTexture("_MainTex", happyTexture);
+                break;
+            case Emotion.Idle:
+                mat.SetTexture("_MainTex", idleTexture);
+                break;
+            case Emotion.IdleTalking:
+                mat.SetTexture("_MainTex", idleTalkingTexture);
+                break;
+
+        }
+    }
+}
