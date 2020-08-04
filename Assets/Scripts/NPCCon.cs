b9b97e715bfb0f1d62f1fef4ead01840813d4659
Blake Howes <blakehowes1@gmail.com>
2020-08-04 14:46:18 +1200

    dsfs

---
+++ Assets/Scripts/NPCCon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCCon : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> Locations = new List<GameObject>();
+    private NavMeshAgent agent;
+    private State MyState;
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(Locations[0].transform.position);
+    }
+
+    public enum State
+    {
+        WALK,
+        IDLE
+    }
+
+    void Update()
+    {
+        switch (MyState)
+        {
+            case State.WALK:
+                Walk();
+                break;
+            case State.IDLE:
+                Idle();
+                break;
+        }
+    }
+
+    private void Walk()
+    {
+
+    }
+
+    private void Idle()
+    {
+
+    }
+}
