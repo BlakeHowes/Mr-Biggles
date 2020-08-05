2f5d874a79403ee70075441279f4f9f1c13d7a17
Blake Howes <blakehowes1@gmail.com>
2020-08-06 00:14:53 +1200

    Things added

---
+++ Assets/Scripts/StartingScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingScript : MonoBehaviour
+{
+    public float FadeSpeed;
+    private bool Fade;
+    public Renderer Shroud;
+    public Renderer Shroud2;
+    private Color theColorToAdjust;
+    public float alpha;
+    void Start()
+    {
+        Color theColorToAdjust = Shroud.material.color;
+        alpha = 1;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Fade = true;
+        }
+    
+        if(Fade == true)
+        {
+            if(alpha > 0f)
+            {
+                alpha -= FadeSpeed;
+                theColorToAdjust.a = alpha;
+                Shroud.material.color = theColorToAdjust;
+                Shroud2.material.color = theColorToAdjust;
+            }
+            if(alpha <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
