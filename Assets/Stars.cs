baca8d2040ddd2993a2397740090de6cb7c2bf64
Blake Howes <blakehowes1@gmail.com>
2020-08-06 18:58:38 +1200

    trefxxxxz

---
+++ Assets/Stars.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stars : MonoBehaviour
+{
+    [SerializeField]
+    private float amount;
+    [SerializeField]
+    public float timer;
+    [SerializeField]
+    public Vector3 startpos;
+    void Start()
+    {
+        startpos = transform.position;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Vector3 offset = new Vector3(amount, amount, amount);
+        transform.position += offset;
+        if(timer < 0.2)
+        {
+            transform.position = startpos + offset;
+        }
+        if (timer > 0.2)
+        {
+            transform.position = startpos - offset;
+        }
+        if(timer > 0.4)
+        {
+            timer = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            SceneManager.LoadScene("Street");
+        }
+    }
+}
